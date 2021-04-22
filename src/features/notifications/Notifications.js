a111100b620d11f38c70bb12e8ae5a56334f4e08
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2021-04-22 10:32:41 +0200

    Notifications - Moment.js and time passed since update

--- src/features/notifications/Notifications.js
+++ src/features/notifications/Notifications.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { Button, StructuredListWrapper, StructuredListHead, StructuredListBody, StructuredListRow, StructuredListCell, Link, Loading } from 'carbon-components-react';
 import { notificationsFetchData } from '../../actions/notifications';
 
@@ -20,24 +21,30 @@ class Notifications extends Component {
     return (
       <div>
         <Button onClick={() => this.testNotifications()}>test</Button>
-        {isLoading ? <Loading description="Active loading indicator" withOverlay={false} /> : null}
-        {hasError ? <p>Sorry! There was an error loading the items</p> : null}
         <StructuredListWrapper selection>
           <StructuredListHead>
             <StructuredListRow head>
-              <StructuredListCell head>{''}</StructuredListCell>
+              <StructuredListCell head>Title</StructuredListCell>
+              <StructuredListCell head>Last updated</StructuredListCell>
             </StructuredListRow>
           </StructuredListHead>
           <StructuredListBody>
             {notifications.map((notification) => (
               <StructuredListRow key={notification.index}>
-                <Link href={notification.url} target='_blank' key={notification.index}>
-                  <StructuredListCell key={notification.index}>{`${notification.title} - updated ${notification.updated_at}`}</StructuredListCell>
-                </Link>
+                <StructuredListCell>
+                  <Link href={notification.url} target='_blank' key={notification.index}>
+                    {notification.title}
+                  </Link>
+                </StructuredListCell>
+                <StructuredListCell>
+                  <time>{moment(notification.updated_at).fromNow()}</time>
+                </StructuredListCell>
               </StructuredListRow>
             ))}
           </StructuredListBody>
         </StructuredListWrapper>
+        {isLoading ? <Loading description="Active loading indicator" withOverlay={false} /> : null}
+        {hasError ? <p>Sorry! There was an error loading the items</p> : null}
       </div>
     );
   }
