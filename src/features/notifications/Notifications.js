e11c736a085c2f872dca021bf9ed93ae3a22e478
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2021-06-07 13:40:45 +0200

    Notifications page - refactoring

--- src/features/notifications/Notifications.js
+++ src/features/notifications/Notifications.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import {
   Button,
@@ -13,25 +12,30 @@ import {
   Loading,
   Tag
 } from 'carbon-components-react';
+import { useHistory } from "react-router-dom";
 import NotificationsSideNav from '../common/NotificationsSideNav';
 import { ExpandAll32 } from '@carbon/icons-react';
-import { withRouter } from "react-router-dom";
 import { fetchNotifications, selectNotification } from '../../actions/notifications';
 
-class Notifications extends Component {
-  constructor(props) {
-    super(props);
-    if (!props.notifications.length) {
-      props.fetchNotifications();
+function Notifications() {
+  const dispatch = useDispatch();
+  const history = useHistory()
+  const notifications = useSelector((state) => state.notifications.notifications);
+  const haveNotificationsError = useSelector((state) => state.notifications.haveNotificationsError);
+  const areNotificationsLoading = useSelector((state) => state.notifications.areNotificationsLoading);
+
+  useEffect(() => {
+    if (!notifications.length) {
+      dispatch(fetchNotifications());
     }
+  }, [notifications, dispatch])
+
+  const selectNotifications = (notification) => {
+    dispatch(selectNotification(notification));
+    history.push('/details');
   }
 
-  selectNotification(notification) {
-    this.props.selectNotification(notification);
-    this.props.history.push('/details');
-  }
-
-  tagReason(reason) {
+  const tagReason = (reason) => {
     switch (reason) {
       case 'review_requested':
         return <Tag type="red" title={reason}>{reason}</Tag>;
@@ -42,81 +46,55 @@ class Notifications extends Component {
     }
   }
 
-  render() {
-    const { notifications, haveNotificationsError, areNotificationsLoading, fetchNotifications } = this.props;
-
-    return (
-      <div className="notifications__main">
-        <div className="notifications__main__content">
-          <Button onClick={() => fetchNotifications()} className="notifications__main__button">Update</Button>
-          <div className="notifications__main__list">
-            <StructuredListWrapper selection>
-              <StructuredListHead>
-                <StructuredListRow head>
-                  <StructuredListCell head>Title</StructuredListCell>
-                  <StructuredListCell head>Last updated</StructuredListCell>
-                  <StructuredListCell head>Reason</StructuredListCell>
-                  <StructuredListCell head>{' '}</StructuredListCell>
+  return (
+    <div className="notifications__main">
+      <div className="notifications__main__content">
+        <Button onClick={() => fetchNotifications()} className="notifications__main__button">Update</Button>
+        <div className="notifications__main__list">
+          <StructuredListWrapper selection>
+            <StructuredListHead>
+              <StructuredListRow head>
+                <StructuredListCell head>Title</StructuredListCell>
+                <StructuredListCell head>Last updated</StructuredListCell>
+                <StructuredListCell head>Reason</StructuredListCell>
+                <StructuredListCell head>{' '}</StructuredListCell>
+              </StructuredListRow>
+            </StructuredListHead>
+            <StructuredListBody>
+              {notifications.map((notification) => (
+                <StructuredListRow key={notification.index}>
+                  <StructuredListCell>
+                    <Link href={notification.html_url} target='_blank' key={notification.index}>
+                      <h6>{notification.full_name}</h6>
+                      <h4>{notification.title}</h4>
+                    </Link>
+                  </StructuredListCell>
+                  <StructuredListCell>
+                    <time>{moment(notification.updated_at).fromNow()}</time>
+                  </StructuredListCell>
+                  <StructuredListCell>
+                    {tagReason(notification.reason)}
+                  </StructuredListCell>
+                  <StructuredListCell>
+                    <Button
+                      kind="tertiary"
+                      renderIcon={ExpandAll32}
+                      iconDescription="Details"
+                      hasIconOnly
+                      onClick={() => {selectNotifications(notification)}}
+                    />
+                  </StructuredListCell>
                 </StructuredListRow>
-              </StructuredListHead>
-              <StructuredListBody>
-                {notifications.map((notification) => (
-                  <StructuredListRow key={notification.index}>
-                    <StructuredListCell>
-                      <Link href={notification.html_url} target='_blank' key={notification.index}>
-                        <h6>{notification.full_name}</h6>
-                        <h4>{notification.title}</h4>
-                      </Link>
-                    </StructuredListCell>
-                    <StructuredListCell>
-                      <time>{moment(notification.updated_at).fromNow()}</time>
-                    </StructuredListCell>
-                    <StructuredListCell>
-                      {this.tagReason(notification.reason)}
-                    </StructuredListCell>
-                    <StructuredListCell>
-                      <Button
-                        kind="tertiary"
-                        renderIcon={ExpandAll32}
-                        iconDescription="Details"
-                        hasIconOnly
-                        onClick={() => {this.selectNotification(notification)}}
-                      />
-                    </StructuredListCell>
-                  </StructuredListRow>
-                ))}
-              </StructuredListBody>
-            </StructuredListWrapper>
-          </div>
-          {areNotificationsLoading ? <Loading description="Active loading indicator" withOverlay /> : null}
-          {haveNotificationsError ? <p>Sorry! There was an error loading the items</p> : null}
+              ))}
+            </StructuredListBody>
+          </StructuredListWrapper>
         </div>
-        <NotificationsSideNav activeLink="notifications" />
+        {areNotificationsLoading ? <Loading description="Active loading indicator" withOverlay /> : null}
+        {haveNotificationsError ? <p>Sorry! There was an error loading the items</p> : null}
       </div>
-    );
-  }
+      <NotificationsSideNav activeLink="notifications" />
+    </div>
+  );
 }
 
-Notifications.propTypes = {
-  notifications: PropTypes.array.isRequired,
-  haveNotificationsError: PropTypes.bool.isRequired,
-  areNotificationsLoading: PropTypes.bool.isRequired
-};
-
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    notifications: state.notifications.notifications,
-    haveNotificationsError: state.notifications.haveNotificationsError,
-    areNotificationsLoading: state.notifications.areNotificationsLoading
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchNotifications: () => dispatch(fetchNotifications()),
-    selectNotification: (notification) => dispatch(selectNotification(notification))
-  };
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Notifications));
+export default Notifications;
