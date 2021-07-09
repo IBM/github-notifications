206d481d3d5aec531837770707386ee0875a7ab9
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2021-07-09 17:01:00 +0200

    Install dotenv

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const GitHubClient = require('../libs/GitHubClient.js').GitHubClient;
 
 export const githubCliEnterprise = new GitHubClient({
   baseUri:"***REMOVED***/api/v3",
-  token:"***REMOVED***"
+  token: process.env.REACT_APP_TOKEN
 });
