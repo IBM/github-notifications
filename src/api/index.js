d1960b7db123e32f6674b01a4fbb932def4540db
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2021-07-14 09:41:19 +0200

    Move directories around

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import { githubToken } from '../utils/authentication';
 
-const GitHubClient = require('../libs/GitHubClient.js').GitHubClient;
+const GitHubClient = require('./libs/GitHubClient.js').GitHubClient;
 
 export const githubCliEnterprise = new GitHubClient({
   baseUri:"***REMOVED***/api/v3",
