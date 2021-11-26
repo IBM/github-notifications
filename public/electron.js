057a62ff27aea33c17d46687434696bae65aed46
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2021-11-26 11:14:45 +0100

    Fix electron startup

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,14 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const isDev = require('electron-is-dev');
 
 let win;
 
 const createWindow = () => {
   win = new BrowserWindow({fullscreen: true});
 
-  win.loadURL('http://localhost:5000');
+  win.loadURL(isDev ? 'http://localhost:5000' : `file://${path.join(__dirname, '../build/index.html')}`)
 
   // win.webContents.openDevTools();
 
