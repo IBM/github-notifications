e962b95301f337eaee2f3a680b346f50f3de13ed
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2022-03-04 15:36:53 +0100

    Add User Profile icon and link to login page

--- src/features/common/NotificationsHeader.js
+++ src/features/common/NotificationsHeader.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { Header, HeaderName, Button, HeaderGlobalBar, HeaderGlobalAction } from "carbon-components-react";
-import { ChevronLeft32, Menu32, Renew16 } from '@carbon/icons-react';
+import { ChevronLeft32, Menu32, Renew16, UserAvatarFilledAlt32 } from '@carbon/icons-react';
 import NotificationsSideNav from './NotificationsSideNav';
 
 const NotificationsHeader = ({ activeLink, dateFilter, autoRefreshView, getItems, notificationItems, notificationsloading }) => {
@@ -9,6 +10,7 @@ const NotificationsHeader = ({ activeLink, dateFilter, autoRefreshView, getItems
     () => setIsToggled(!isToggled),
     [isToggled, setIsToggled],
   );
+  const history = useHistory()
 
   return (
   <Header aria-label="Github Notifications" className="notifications__header">
@@ -24,16 +26,21 @@ const NotificationsHeader = ({ activeLink, dateFilter, autoRefreshView, getItems
     <HeaderName prefix="Github">
       Notifications
     </HeaderName>
-    { autoRefreshView && (
       <HeaderGlobalBar>
-        <HeaderGlobalAction onClick={autoRefreshView} aria-label="REFRESH">
-          <Renew16 className={notificationsloading ? 'notifications__header__refresh-icon--spin' : ''} />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction onClick={getItems} aria-label="NEW">
-          <div className="notifications__header__new-notifications-icon">{ notificationItems.length }</div>
+        { autoRefreshView && (
+          <React.Fragment>
+            <HeaderGlobalAction onClick={autoRefreshView} aria-label="REFRESH">
+              <Renew16 className={notificationsloading ? 'notifications__header__refresh-icon--spin' : ''} />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction onClick={getItems} aria-label="NEW">
+              <div className="notifications__header__new-notifications-icon">{ notificationItems.length }</div>
+            </HeaderGlobalAction>
+          </React.Fragment>
+        )}
+        <HeaderGlobalAction onClick={() => history.push('/')} aria-label="LOG OUT">
+          <UserAvatarFilledAlt32 />
         </HeaderGlobalAction>
       </HeaderGlobalBar>
-    )}
     <NotificationsSideNav activeLink={activeLink} dateFilter={dateFilter} isSideNavExpanded={isToggled}/>
   </Header>
   )}
