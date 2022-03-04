21a0ab6c05f53b4790baae7b4007bae056e6f081
Grzegorz-Smolin <grzegorz.smolin@ibm.com>
2022-03-04 15:36:29 +0100

    Dont require inputting api/v3 on the login page

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,6 @@ import { githubUrl, githubToken } from '../utils/authentication';
 const GitHubClient = require('./libs/GitHubClient.js').GitHubClient;
 
 export const githubCliEnterprise = new GitHubClient({
-  baseUri: githubUrl,
+  baseUri: `${githubUrl}/api/v3`,
   token: githubToken
 });
